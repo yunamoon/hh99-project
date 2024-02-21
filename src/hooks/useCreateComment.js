5686946e41f080f356bb14dda28d8001f941e564
yunamoon <moon@mun-yunas-MacBook-Pro.local>
2024-02-21 22:26:32 +0900

    update post contents -useLike, useCreateComment

--- src/hooks/useCreateComment.js
+++ src/hooks/useCreateComment.js
@@ -1,37 +1,38 @@
 import { useState } from "react";
 import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
-import  postSlice  from '@/store/post.slice.jsx';
-import  userSlice  from "../store/user.sliec";
+import { addComment } from '@/store/post.slice'; // 수정된 부분
+import { useSelector } from 'react-redux'; // 수정된 부분
 
 const useCreateComment = () => {
-	const [isCommenting, setIsCommenting] = useState(false);
-	const authUser = userSlice((state) => state.user);
-	const addComment = postSlice((state) => state.addComment);
+    const [isCommenting, setIsCommenting] = useState(false);
+    const authUser = useSelector((state) => state.user); // 수정된 부분
 
-	const handlePostComment = async (postId, comment) => {
-		if (isCommenting) return;
-		if (!authUser) return showToast("Error", "You must be logged in to comment", "error");
-		setIsCommenting(true);
-		const newComment = {
-			comment,
-			createdAt: Date.now(),
-			createdBy: authUser.uid,
-			postId,
-		};
-		try {
-			await updateDoc(doc(db, "posts", postId), {
-				comments: arrayUnion(newComment),
-			});
-			addComment(postId, newComment);
-		} catch (error) {
-			showToast("Error", error.message, "error");
-		} finally {
-			setIsCommenting(false);
-		}
-	};
+    const handlePostComment = async (postId, comment) => {
+        if (isCommenting) return;
+        if (!authUser) return showToast("Error", "You must be logged in to comment", "error");
+        setIsCommenting(true);
+     
+        const newComment = {
+            comment,
+            createdAt: Date.now(),
+            createdBy: authUser.uid,
+            postId,
+        };
+     
+        try {
+            await updateDoc(doc(db, "posts", postId), {
+                comments: arrayUnion(newComment),
+            });
+            addComment({ postId, comment: newComment }); // 수정된 부분
+        } catch (error) {
+            showToast("Error", error.message, "error");
+        } finally {
+            setIsCommenting(false);
+        }
+    };
 
-	return { isCommenting, handlePostComment };
+    return { isCommenting, handlePostComment };
 };
 
-export default useCreateComment
+export default useCreateComment;
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { auth, db} from '@/firebase'
+import { auth, db} from '@/firebase/firebase'
 import { useDispatch } from 'react-redux';
 import { setUser } from '@/store/user.sliec.jsx';
 

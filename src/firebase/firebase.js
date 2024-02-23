3684c5dcc7c0179dbd6aaf5d04d32962a9ddbfe2
yunamoon <moon@mun-yunas-MacBook-Pro.local>
2024-02-24 03:19:38 +0900

    .env update, secrets key update

---
+++ src/firebase/firebase.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/storage'
+import 'firebase/compat/firestore'
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth ,updateProfile} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth(app);
+const storage = firebase.storage();
+const db = firebase.firestore();
+
+export { auth, storage, db };

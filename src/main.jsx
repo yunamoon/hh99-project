077a0ce676738a7ab0f1e59711071da6eb880a2a
yunamoon <tpwkao0051@gmail.com>
2024-02-24 05:09:12 +0900

    .env update

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx'; // 경로 수정
-import './index.css'; // 경로 수정
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import { Provider } from 'react-redux'; 
-import store from './store/index.jsx'; // 경로 수정
+import store from '@/store/index.jsx'; 
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
 );

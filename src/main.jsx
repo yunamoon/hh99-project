04e8345bf881d8320792441ffd503137420e3a9c
yunamoon <moon@mun-yunas-MacBook-Pro.local>
2024-02-24 04:12:18 +0900

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import ReactDOM from 'react-dom';
+import App from './App.jsx'; // 경로 수정
+import './index.css'; // 경로 수정
 import { Provider } from 'react-redux'; 
-import store from '@/store/index.jsx'; 
+import store from './store/index.jsx'; // 경로 수정
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
+  document.getElementById('root')
 );

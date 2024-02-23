9d928d9f9fa7f81fc90f24e7b8d70bdc1e3c17ea
yunamoon <moon@mun-yunas-MacBook-Pro.local>
2024-02-24 03:56:22 +0900

    path update 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/App.jsx';
-import '@/index.css';
-import { Provider } from 'react-redux'; // Redux의 Provider를 가져옵니다.
-import store from '@/store/index.jsx'; // Redux 스토어를 가져옵니다.
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux'; 
+import store from '@/store/index.jsx'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

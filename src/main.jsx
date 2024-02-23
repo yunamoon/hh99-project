67306c700b7adb7b7c4fd49a677085291cf944d6
yunamoon <moon@mun-yunas-MacBook-Pro.local>
2024-02-24 03:50:36 +0900

    path update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import App from '@/App.jsx';
+import '@/index.css';
 import { Provider } from 'react-redux'; // Redux의 Provider를 가져옵니다.
 import store from '@/store/index.jsx'; // Redux 스토어를 가져옵니다.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Provider로 App 컴포넌트를 감싸줍니다. */}
     <Provider store={store}>
       <App />
     </Provider>

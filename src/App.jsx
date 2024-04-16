c18b264b41c1bf5df7611ba4c8d9624a0224f2c5
yunamoon <tpwkao0051@gmail.com>
2024-04-17 00:41:49 +0900

    guest permission update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter , Route, Routes} from "react-router-dom";
-
-// Home , Auth, layout , mypage
 import PageLayout from "@/Layouts/PageLayout.jsx";
 import HomePage from '@/pages/HomePage/index';
 import MyPage from '@/pages/MyPage/index';
